78f4b49dbbde6792563a30a9bb63861edbea688f
dvolk <danik.jb@mail.ru>
2022-02-08 21:37:03 +0300

    Сделал тайм и таск трекер

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
 import telebot
 from telebot import types
-
-token = "5174930087:AAGoeno-wC93dPb-_z_yCdoHRf_JbqyeZYI"
-bot = telebot.TeleBot(token, parse_mode=None)
+import userstats
+import time
+TOKEN = "5174930087:AAGoeno-wC93dPb-_z_yCdoHRf_JbqyeZYI"
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 phrases = ["Ты великолепен!!!", "Под твоей клавиатурой находится меню. Пользуйся!!", "Нажми кнопку Трекер чтобы "
                                                                                      "начать решать задачи. Для "
                                                                                      "получения аналитики нажмите "
-                                                                                     "Статистика. Удачи!!!"]
+                                                                                    "Статистика. Удачи!!!"]
+
+def spawn_tracker(stats, call=None,message=None):
+    if call is None:
+
+        msg = "Таймер {}. \nВы кодите: {} секунд \nРешено: {} задачи".format(
+            "запущен" if stats["timer_state"] else "выключен", stats["timer_count"], stats["task_count"])
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        remove_button = types.InlineKeyboardButton("Удалить задачу", callback_data="remove_task")
+        change_task_state_button = types.InlineKeyboardButton(
+            "Закончить решать задачу" if stats["timer_state"] else "Начать решать задачу",
+            callback_data="change_timer_state")
+        markup.add(remove_button)
+        markup.add(change_task_state_button)
+        bot.send_message(message.chat.id, msg, reply_markup=markup)
+    elif message is None:
+
+        msg = "Таймер {}. \nВы кодите: {} секунд \nРешено: {} задачи".format(
+            "запущен" if stats["timer_state"] else "выключен", stats["timer_count"], stats["task_count"])
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        remove_button = types.InlineKeyboardButton("Удалить задачу", callback_data="remove_task")
+        change_task_state_button = types.InlineKeyboardButton(
+            "Закончить решать задачу" if stats["timer_state"] else "Начать решать задачу",
+            callback_data="change_timer_state")
+        markup.add(remove_button)
+        markup.add(change_task_state_button)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=msg,
+                              reply_markup=markup)
 
 
+@bot.message_handler(commands=['save'])
+def savebot(message):
+    bot.send_message(message.chat.id, "Сохраняемся.....")
+    userstats.save()
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    # Приветственные сообщения + найстройка меню клавиатуры
+    # найстройка меню клавиатуры
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     statistics_button = types.KeyboardButton("Статистика")
     tracker_button = types.KeyboardButton("Трекер")
     markup.row(statistics_button, tracker_button)
+    # Приветственные сообщения
     bot.send_message(message.chat.id, phrases[0])
     bot.send_message(message.chat.id, phrases[1])
     bot.send_message(message.chat.id, phrases[2], reply_markup=markup)
+    # Регистрация нового пользователя
+    if not userstats.check_user_registration(message.from_user):
+        userstats.add_new_user(message.from_user)
 
 
 @bot.message_handler(content_types='text')
@@ -41,7 +77,37 @@ def message_reply(message):
         bot.send_message(message.chat.id, phrases[2], reply_markup=markup)
     elif message.text == "Трекер":
         # Призыв трекера
-        bot.send_message(message.chat.id, "Таймер запущен. \nВы кодите: 15 мин \nРешено: 3 задачи")
+        stats = userstats.get_stat_by_id(message.from_user)
+        userstats.update_timer(message.from_user)
+        spawn_tracker(stats, message=message)
 
 
-bot.polling()
+@bot.callback_query_handler(lambda m: True)
+def query_handler(call):
+    if call.message:
+        # Функция удаления задач
+        if call.data == "remove_task":
+            # Проверка что задач не ноль, чтобы телеграм не ругался
+            if userstats.decrease_user_task_count(call.message.chat):
+                stats = userstats.get_stat_by_id(call.message.chat)
+                spawn_tracker(stats, call)
+
+        # Изменение состояния таймера + трекинг задач
+        elif call.data == "change_timer_state":
+            stats = userstats.get_stat_by_id(call.message.chat)
+            if stats["timer_state"]:
+                userstats.increase_user_task_count(call.message.chat)
+                userstats.update_timer(call.message.chat)
+            else:
+                userstats.start_timer(call.message.chat)
+            userstats.change_task_state(call.message.chat)
+            spawn_tracker(stats, call=call)
+
+
+try:
+    userstats.load()
+    bot.polling()
+except ZeroDivisionError:
+    userstats.save()
+finally:
+    userstats.save()
